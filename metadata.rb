30d9a956ac3f2d4bfa98c97766417eb48edf5f69
Mike Fiedler <miketheman@gmail.com>
2012-02-17 22:07:14 -0800

    Reverting maintainer change made in #1

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
-maintainer        "Ryan Roemer"
-maintainer_email  "ryan@loose-bits.com"
+maintainer        "Mike Fiedler"
+maintainer_email  "miketheman@gmail.com"
 license           "Apache 2.0"
 description       "Installs and configures the Scout Agent"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))

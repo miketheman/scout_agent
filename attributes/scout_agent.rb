2592a353b57c5435dff0be746b97c4395e5a99c6
Mike Fiedler <miketheman@gmail.com>
2012-07-06 21:26:46 -0400

    updated attribute file to include license, use default vs set_unless

--- attributes/scout_agent.rb
+++ attributes/scout_agent.rb
@@ -1,5 +1,24 @@
-set_unless['scout_agent']['user'] = "scout"
-set_unless['scout_agent']['group'] = "scout"
-set_unless['scout_agent']['version'] = "5.5.4"
-set_unless['scout_agent']['scout_bin'] = "/usr/bin/scout"
-set_unless['scout_agent']['rename'] = false
+#
+# Cookbook Name:: scout_agent
+# Attributes:: default
+#
+# Copyright 2011-2012, Michael A. Fiedler
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+default['scout_agent']['user'] = "scout"
+default['scout_agent']['group'] = "scout"
+default['scout_agent']['uid'] = 999
+default['scout_agent']['gid'] = 999
+default['scout_agent']['version'] = "5.5.5"
+default['scout_agent']['rename'] = false
